c061a8802ea148f900a243153575fe6f3926dbb3
P__.mp4 <postlist.ed@gmail.com>
2022-08-21 07:05:46 +0900

    2022-08-21 07:05 최초commit

---
+++ src/main/java/com/pmp4/studyspace/StudySpaceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pmp4.studyspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpaceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpaceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmp4/studyspace/StudySpaceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmp4.studyspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpaceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
